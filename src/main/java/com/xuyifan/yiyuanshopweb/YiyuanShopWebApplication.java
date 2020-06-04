5e3fbb52954d158dfb44d3b4f02f5f0540af4ff7
xuyifan723 <446903623@qq.com>
2020-06-04 23:44:14 +0800

    sss

---
+++ src/main/java/com/xuyifan/yiyuanshopweb/YiyuanShopWebApplication.java
@@ -0,0 +1,13 @@
+package com.xuyifan.yiyuanshopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiyuanShopWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiyuanShopWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuyifan/yiyuanshopweb/YiyuanShopWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuyifan.yiyuanshopweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YiyuanShopWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
